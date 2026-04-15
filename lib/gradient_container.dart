c0a7d7706d6d8f3e386f6beb71983e6ce9936d1f
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-15 14:26:33 +0300

    Extract StyledText into a separate widget and file and Make StyledText reusable by accepting text as a parameter

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab3_app/styled_text.dart';
+
+const startAlignment = Alignment.topCenter;
+const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.white,
+            Colors.blue,
+            Colors.red,
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: StyledText("Hello world!"),
+        ),
+      );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab3_app/gradient_container.dart';
+import 'package:flutter_lab3_app/styled_text.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: (Scaffold(
-        body: GradientContainer()
+        body: GradientContainer(
+          
+        )
         )),
       ),
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.white,
-            Colors.blue,
-            Colors.red,
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Center(
-        child: Text(
-          "Hello world!",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-          ),
-        ),
-      ),
-    );
-  }
-}
+
