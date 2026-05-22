32aae7ba4e09d20f83aad22d06fe6f7e02ff6242
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-22 17:58:23 +0300

    Refactor dice logic into StatefulWidget DiceRoller

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab3_app/styled_text.dart';
+import 'package:flutter_lab3_app/dice_roller.dart';
+import 'package:flutter_lab3_app/dice_roller.dart';
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
@@ -16,13 +17,6 @@ class GradientContainer extends StatelessWidget {
     {super.key}
   );
 
-  var activeDiceImage = "assets/images/dice-1.png";
-
-  void rollDice() {
-    activeDiceImage = "assets/images/dice-4.png";
-    print("Изменили картинку");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,29 +28,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 300,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                ),
-                foregroundColor: Colors.lime,
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              child: Text("Roll Dice"),
-            ),
-          ],
-        ),
+        child: DiceRoller()
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab3_app/gradient_container.dart';
-import 'package:flutter_lab3_app/styled_text.dart';
 
 void main() {
   runApp(
